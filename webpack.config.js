945b3f99f96d3bf7195f82a7a896b3d2dd827480
artemplv <artemplv@gmail.com>
2021-03-23 21:07:04 +0300

    add docker

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"]
   },
+  devServer: {
+    contentBase: "dist",
+    compress: true,
+    host: "0.0.0.0",
+    historyApiFallback: true,
+    port: 3000
+  },
   module: {
     rules: [
       {
