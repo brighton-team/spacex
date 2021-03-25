cb6a8a86a21f2cb3e01a8b90d53eb1cb9ccf2bf7
Alexey Mavrichev <a@grebennikov.de>
2021-03-25 22:29:07 +0300

    Before push

--- src/components/ActionButton/index.tsx
+++ src/components/ActionButton/index.tsx
@@ -2,22 +2,18 @@ import React, { PropsWithChildren } from 'react';
 import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
 
-
-
 const StyledButton = styled(Button)`
-  border: 1px solid #FFFFFF;
-  boxSizing: border-box;
-  borderRadius: 2px;
+  border: 1px solid #ffffff;
+  boxsizing: border-box;
+  borderradius: 2px;
   color: white;
-  fontWeight: 700;
+  fontweight: 700;
   padding: 5px 24px 0px 24px;
-  fontSize: 16px;
+  fontsize: 16px;
   height: 33px;
 `;
 
-export function ActionButton(
-  props: PropsWithChildren<{ customClass: string }>
-): JSX.Element {
+export function ActionButton(props: PropsWithChildren<{ customClass: string }>): JSX.Element {
   const { customClass, children } = props;
   return <StyledButton className={`${customClass}`}>{children}</StyledButton>;
 }
