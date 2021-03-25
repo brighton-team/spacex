cd7ba7db2dd17fd8d2980ea49326cbefa57878df
Alexey Mavrichev <a@grebennikov.de>
2021-03-25 23:23:34 +0300

    Webpack wrong Login entry fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/Login.tsx',
+  entry: './src/pages/Login/Login.tsx',
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
