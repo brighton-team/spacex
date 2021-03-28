fe2fe277e936a2d714104f5557ea8453b0c5f37e
Oleg Ryazantsev <osryazantsev@gmail.com>
2021-03-28 23:27:56 +0300

    add(Login): add API and axios

--- src/consts/routes.ts
+++ src/consts/routes.ts
@@ -3,3 +3,8 @@ export const signIn = '/signin';
 export const leaders = '/leaders';
 export const forum = '/forum';
 export const profile = '/profile';
+
+const BASE_URL = 'https://ya-praktikum.tech/api/v2';
+export const BASE_AUTH_URL = `${BASE_URL}/auth`;
+export const SIGN_UP_URL = `${BASE_AUTH_URL}/signup`;
+export const SIGN_IN_URL = `${BASE_AUTH_URL}/signin`;
