3aaf5393f1a2663e25dfae67dd74ada6d0efb4f0
Oleg Ryazantsev <osryazantsev@gmail.com>
2021-04-04 20:38:39 +0300

    add redux

---
+++ src/store/actionTypes.ts
@@ -0,0 +1,8 @@
+const REQUEST = '_REQUEST';
+const SUCCESS = '_SUCCESS';
+const FAILURE = '_FAILURE';
+
+const SIGN_IN = 'SIGN_IN';
+export const SIGN_IN_REQUEST = `${SIGN_IN}${REQUEST}`;
+export const SIGN_IN_SUCCESS = `${SIGN_IN}${SUCCESS}`;
+export const SIGN_IN_FAILURE = `${SIGN_IN}${FAILURE}`;
---
+++ src/types/actionTypes.ts
@@ -0,0 +1,19 @@
+export interface IUser {
+  id?: number | null;
+  first_name?: string;
+  second_name?: string;
+  display_name?: string;
+  login?: string;
+  email?: string;
+  phone?: string;
+  avatar?: string;
+}
+
+export type SignInAction = {
+  type: string;
+  payload: IUser;
+};
+
+export type UserState = {
+  user: IUser;
+};
