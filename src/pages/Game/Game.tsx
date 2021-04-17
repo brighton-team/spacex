e55084231759aad45c13e3fd3473cc4a24a453b8
Alexey Mavrichev <a@grebennikov.de>
2021-04-17 15:25:38 +0300

    button added

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -3,10 +3,28 @@ import React, { useEffect, useCallback } from 'react';
 import { GameModal, openModal, closeModal } from 'components/GameModal';
 import { connect } from 'react-redux';
 import { IGameModal } from 'types/actionTypes';
-import { PageWrapper, PauseButton, Canvas } from './styledItems';
+import { PageWrapper, PauseButton, Canvas, FullscreenButton, expand, reduce } from './styledItems';
 import { GameLogic } from './logic/GameLogic';
 
 export const GameComponent = (props: OwnProps): JSX.Element => {
+
+ 
+
+const toggleFullScreen = (e:React.MouseEvent) => {
+  const button = e.target as HTMLHtmlElement;
+  button.blur();
+  if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+      button.style.backgroundImage = `url(${reduce})`;
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+      button.style.backgroundImage = `url(${expand})`;
+    }
+  }
+}
+
+
   const openModalCallback = useCallback(openModal, []);
   const closeModalCallback = useCallback(closeModal, []);
   useEffect(() => {
@@ -23,6 +41,7 @@ export const GameComponent = (props: OwnProps): JSX.Element => {
     <PageWrapper>
       <Canvas id="game" />
       <PauseButton onClick={openModalCallback} />
+      <FullscreenButton onClick={toggleFullScreen} />
       <GameModal isVisible={gameModal.isVisible} onClose={closeModalCallback} />
     </PageWrapper>
   );
