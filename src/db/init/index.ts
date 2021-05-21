32772b658038cc94f060eab90f42524c15a1d58d
Alexey Mavrichev <a@grebennikov.de>
2021-05-21 20:44:30 +0300

    models

--- src/db/init/index.ts
+++ src/db/init/index.ts
@@ -3,6 +3,8 @@ import { User } from '../models/user';
 import { Topic } from '../models/topic';
 // eslint-disable-next-line import/no-cycle
 import { Post } from '../models/post';
+import { Theme } from '../models/theme';
+import { UserTheme } from '../models/userTheme';
 
 const sequelizeOptions: SequelizeOptions = {
   host: 'postgres',
@@ -27,9 +29,13 @@ const sequelize = new Sequelize(
 const userModel = sequelize.define('user', User);
 const topicModel = sequelize.define('topic', Topic);
 const postModel = sequelize.define('post', Post);
+const themeModel = sequelize.define('theme', Theme);
+const userThemeModel = sequelize.define('user_theme', UserTheme);
 
 userModel.hasMany(topicModel);
 userModel.hasMany(postModel);
 topicModel.hasMany(postModel);
+themeModel.hasOne(userThemeModel, { foreignKey: 'themeId' });
+userThemeModel.belongsTo(themeModel, { foreignKey: 'themeId' });
 
 export default sequelize;
